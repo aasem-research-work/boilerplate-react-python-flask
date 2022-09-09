a22bdba95f717e73f9629afcd8b7db18ee73bba6
aasem <aasem.research.work@gmail.com>
2022-09-09 09:37:15 +0500

    changed

--- app.py
+++ app.py
@@ -2,13 +2,46 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 #from flask_cors import CORS #comment this on deployment
 from api.HelloApiHandler import HelloApiHandler
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 #CORS(app) #comment this on deployment
 api = Api(app)
-
+#route
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
+function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
+      </header>
+    </div>
+  );
+}
+
 api.add_resource(HelloApiHandler, '/flask/hello');
+
+export default App;
